5ffc46897fecc5df998e7030a75c43ea271fdc6a
Sriramjupally <cs21btech11025@iith.ac.in>
2024-04-11 14:51:09 +0530

    Hello world

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-hi everyone!!
-dhfleqjkwabf rvelkahdnfg veqlawkhfnd vlqevjkdasxf v4eqilwhsdtn fgleridtng 43rweljdkstfgniueqrwjklads gf
-ewqdljhskagf leqwda
-
-hje,whribuflewab rfb4leiqwuahf qewa
+int main(){
+    return 0;
+}
